bac297fe6ce52270c8a69479587f21742ad67e9e
IRONM00N <me@ironmoon.dev>
2025-01-10 22:57:02 -0500

    flake: follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -21,6 +21,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
